47b6880459e3600605298e723a757780c0c7cfee
Atila Neves <aalvesne@cisco.com>
2015-02-09 19:28:41 +0100

    Removed dependency on rdmd

--- src/reggae_main.d
+++ src/reggae_main.d
@@ -3,6 +3,9 @@ import std.stdio;
 import std.process: execute;
 import std.array: array, join;
 import std.path: absolutePath;
+import std.algorithm: map;
+import std.array;
+
 
 int main(string[] args) {
     {
@@ -10,12 +13,22 @@ int main(string[] args) {
         file.write(import("run_main.d"));
     }
 
-    const path = args[1];
-    auto cmd = ["rdmd", "--chatty", "-I" ~ path, "-I~/coding/d/reggae/src",
-                "tmpfile.d", path];
-    auto ret = execute(cmd);
-    if(ret.status != 0) {
-        stderr.writeln("Couldn't execute ", cmd.join(" "), ":\n", ret.output);
+    const projectPath = args[1];
+    const reggaeSrcs = ["backend.d", "build.d", "makefile.d",
+                        "package.d", "range.d", "reflect.d"].
+        map!(a => "/home/aalvesne/coding/d/reggae/src/reggae/" ~ a).array;
+    auto compile = ["dmd", "-g", "-debug","-I" ~ projectPath, "-I/home/aalvesne/coding/d/reggae/src",
+                    "-ofbuild", "tmpfile.d",
+                    buildPath(projectPath, "reggaefile.d")] ~ reggaeSrcs;
+    auto retComp = execute(compile);
+    if(retComp.status != 0) {
+        stderr.writeln("Couldn't execute ", compile.join(" "), ":\n", retComp.output);
+        return 1;
+    }
+
+    auto retRun = execute(["./build", projectPath]);
+    if(retRun.status != 0) {
+        stderr.writeln("Couldn't execute the produced build binary:\n", retRun.output);
         return 1;
     }
 

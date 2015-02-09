88061f56b8cdf1c062ddea6b2c6fe5eb705be219
Atila Neves <aalvesne@cisco.com>
2015-02-09 19:46:39 +0100

    No hard-coded import path for reggae src files

--- src/reggae/reggae_main.d
+++ src/reggae/reggae_main.d
@@ -3,23 +3,24 @@ import std.stdio;
 import std.process: execute;
 import std.array: array, join;
 import std.path: absolutePath;
-import std.algorithm: map;
-import std.array;
+import std.typetuple;
 
 
 int main(string[] args) {
-    {
-        auto file = File("tmpfile.d", "w");
-        file.write(import("run_main.d"));
+    alias fileNames = TypeTuple!("run_main.d", "backend.d", "build.d",
+                                 "makefile.d",
+                                 "package.d", "range.d", "reflect.d");
+    writeSrcFiles!(fileNames);
+    string[] reggaeSrcs;
+    foreach(fileName; fileNames) {
+        reggaeSrcs ~= fileName;
     }
 
     const projectPath = args[1];
-    const reggaeSrcs = ["backend.d", "build.d", "makefile.d",
-                        "package.d", "range.d", "reflect.d"].
-        map!(a => "/home/aalvesne/coding/d/reggae/src/reggae/" ~ a).array;
-    auto compile = ["dmd", "-g", "-debug","-I" ~ projectPath, "-I/home/aalvesne/coding/d/reggae/src",
-                    "-ofbuild", "tmpfile.d",
+    auto compile = ["dmd", "-g", "-debug","-I" ~ projectPath, "-I.",
+                    "-ofbuild",
                     buildPath(projectPath, "reggaefile.d")] ~ reggaeSrcs;
+
     auto retComp = execute(compile);
     if(retComp.status != 0) {
         stderr.writeln("Couldn't execute ", compile.join(" "), ":\n", retComp.output);
@@ -33,4 +34,12 @@ int main(string[] args) {
     }
 
     return 0;
+}
+
+
+void writeSrcFiles(fileNames...)() {
+    foreach(fileName; fileNames) {
+        auto file = File(fileName, "w");
+        file.write(import(fileName));
+    }
 }

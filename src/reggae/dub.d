a0f07f7dbf04105c422d8ec1709ef7ed2a5f95d4
Atila Neves <aalvesne@cisco.com>
2015-02-20 15:20:16 +0000

    Refactored dubInfo

--- src/reggae/dub.d
+++ src/reggae/dub.d
@@ -17,13 +17,18 @@ struct DubPackage {
 
 DubInfo dubInfo(string jsonString) @safe {
     auto json = parseJSONValue(jsonString);
-    auto packages = json.get!(JSONValue[string])["packages"].get!(JSONValue[]);
-    return DubInfo(packages.map!(a => DubPackage(a.get!(JSONValue[string])["name"].get!string,
-                                                 a.get!(JSONValue[string])["path"].get!string,
-                                                 a.get!(JSONValue[string])["files"].get!(JSONValue[]).jsonValueToFiles)).array);
+    auto packages = json.byKey("packages").get!(JSONValue[]);
+    return DubInfo(packages.map!(a => DubPackage(a.byKey("name").get!string,
+                                                 a.byKey("path").get!string,
+                                                 a.byKey("files").get!(JSONValue[]).jsonValueToFiles)).array);
 }
 
 
 private string[] jsonValueToFiles(JSONValue[] files) @safe {
-    return files.map!(a => a.get!(JSONValue[string])["path"].get!string).array;
+    return files.map!(a => a.byKey("path").get!string).array;
+}
+
+
+auto byKey(JSONValue json, in string key) @safe {
+    return json.get!(JSONValue[string])[key];
 }

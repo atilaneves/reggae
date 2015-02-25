83d7b50a14dbae726b8781c1ed033838204bb3b3
Atila Neves <aalvesne@cisco.com>
2015-02-25 10:16:47 +0000

    Fixed breakage of other tests that don't depend on dub

--- src/reggae/reggae_main.d
+++ src/reggae/reggae_main.d
@@ -114,19 +114,21 @@ void writeSrcFiles(fileNames...)(in Options options) {
         file.writeln("import reggae.dub;");
         file.writeln("immutable projectPath = `", options.projectPath, "`;");
 
-        import std.process;
-        const string[string] env = null;
-        Config config = Config.none;
-        size_t maxOutput = size_t.max;
-        immutable workDir = options.projectPath;
+        if(isDubProject(options.projectPath)) {
+            import std.process;
+            const string[string] env = null;
+            Config config = Config.none;
+            size_t maxOutput = size_t.max;
+            immutable workDir = options.projectPath;
 
-        immutable dubArgs = ["dub", "describe"];
-        immutable ret = execute(dubArgs, env, config, maxOutput, workDir);
-        enforce(ret.status == 0, text("Could not get description from dub with ", dubArgs, ":\n",
-                                      ret.output));
+            immutable dubArgs = ["dub", "describe"];
+            immutable ret = execute(dubArgs, env, config, maxOutput, workDir);
+            enforce(ret.status == 0, text("Could not get description from dub with ", dubArgs, ":\n",
+                                          ret.output));
 
-        auto dubInfo = dubInfo(ret.output);
-        file.writeln("const dubInfo = ", dubInfo, ";");
+            auto dubInfo = dubInfo(ret.output);
+            file.writeln("const dubInfo = ", dubInfo, ";");
+        }
     }
 }
 

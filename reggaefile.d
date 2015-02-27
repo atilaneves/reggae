d965825cb31f43668526d17246fceaf068af262e
Atila Neves <aalvesne@cisco.com>
2015-02-27 13:19:23 +0000

    Deleted reggaefile.d since it no longer applies. Reggae can build itself again, but no UT build

--- reggaefile.d
+++
@@ -1,6 +0,0 @@
-import reggae;
-
-mixin dExe!(App("src/reggae/reggae_main.d", "reggae"),
-            Flags("-g -debug"),
-            ImportPaths(["src"]),
-            StringImportPaths(["src/reggae"]));

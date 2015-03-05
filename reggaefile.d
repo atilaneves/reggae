590f9f0469d4d7417c6d6504839ec714cc38c864
Atila Neves <aalvesne@cisco.com>
2015-03-05 15:59:34 +0000

    Comments added to reggaefile.d

--- reggaefile.d
+++ reggaefile.d
@@ -1,7 +1,9 @@
 import reggae;
 
+//the actual reggae binary
 alias main = dubMainTarget!("-g -debug");
 
+//the unit test binary
 alias utObjs = dObjects!(SrcDirs([`tests`]), Flags(`-unittest`), ImportPaths(dubInfo.targetImportPaths));
 alias ut = dExeWithDubObjs!("ut", utObjs);
 

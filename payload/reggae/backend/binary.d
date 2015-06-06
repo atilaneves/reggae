9ae51a2a8408836806e99347cada3923c656a7f0
Atila Neves <aalvesne@cisco.com>
2015-06-06 21:21:56 +0200

    cppcompile.feature works, including automatic dependencies

--- payload/reggae/backend/binary.d
+++ payload/reggae/backend/binary.d
@@ -5,17 +5,17 @@ import reggae.build;
 import reggae.range;
 import reggae.config;
 import std.algorithm: all, splitter, cartesianProduct, any, filter;
-import std.range: chain;
+import std.range;
 import std.file: timeLastModified, thisExePath, exists;
 import std.process: execute, executeShell;
 import std.path: absolutePath;
 import std.typecons: tuple;
-import std.exception: enforce;
+import std.exception;
 import std.stdio;
 import std.parallelism: parallel;
-import std.conv: text;
+import std.conv;
 import std.array: replace, empty;
-import std.string: stripLeft;
+import std.string: strip;
 
 @safe:
 
@@ -37,7 +37,10 @@ struct Binary {
                 foreach(target; level.parallel) {
 
                     const outs = target.outputsInProjectPath(projectPath);
-                    //if((outs[0] ~ ".d").exists) writeln("A dep file for ", outs[0]);
+                    immutable depFileName = outs[0] ~ ".dep";
+                    if(depFileName.exists) {
+                        didAnything = checkDeps(target, depFileName) || didAnything;
+                    }
 
                     didAnything = checkTarget(target) || didAnything;
                 }
@@ -76,22 +79,36 @@ private:
     }
 
     bool checkTarget(in Target target) const {
-        bool didAnything;
         foreach(dep; chain(target.dependencies, target.implicits)) {
             if(cartesianProduct(dep.outputsInProjectPath(projectPath),
                                 target.outputsInProjectPath(projectPath)).
                any!(a => a[0].newerThan(a[1]))) {
 
-                didAnything = true;
                 mkDir(target);
                 immutable cmd = target.shellCommand(projectPath);
                 writeln("[build] " ~ cmd);
                 immutable res = executeShell(cmd);
                 enforce(res.status == 0, "Could not execute " ~ cmd ~ ":\n" ~ res.output);
+                return true;
             }
         }
 
-        return didAnything;
+        return false;
+    }
+
+    bool checkDeps(in Target target, in string depFileName) const @trusted {
+        auto file = File(depFileName);
+        const dependencies = file.byLine.map!(a => a.to!string).dependenciesFromFile;
+        if(dependencies.any!(a => a.newerThan(target.outputsInProjectPath(projectPath)[0]))) {
+            mkDir(target);
+            immutable cmd = target.shellCommand(projectPath);
+            writeln("[build] " ~ cmd);
+            immutable res = executeShell(cmd);
+            enforce(res.status == 0, "Could not execute " ~ cmd ~ ":\n" ~ res.output);
+
+            return true;
+        }
+        return false;
     }
 }
 
@@ -113,11 +130,10 @@ private void mkDir(in Target target) @trusted {
     }
 }
 
-string[] dependenciesFromFile(in string output) pure {
-    return output.
-        splitter("\n").
+string[] dependenciesFromFile(R)(R lines) @trusted if(isInputRange!R) {
+    return lines.
         map!(a => a.replace(" \\", "")).
         filter!(a => !a.empty).
-        map!(a => a.stripLeft).
+        map!(a => a.strip).
         array[1..$];
 }
--- tests/binary.d
+++ tests/binary.d
@@ -5,10 +5,11 @@ import unit_threaded;
 
 
 void testDep() {
-    immutable depFileString = "objs/calc.objs/src/cpp/maths.o: \\\n
- /home/aalvesne/coding/d/reggae/tmp/aruba/mixproj/src/cpp/maths.cpp \\\n
- /home/aalvesne/coding/d/reggae/tmp/aruba/mixproj/headers/maths.hpp\n";
-    dependenciesFromFile(depFileString).shouldEqual(
+    immutable depFileLines = [
+        "objs/calc.objs/src/cpp/maths.o: \\",
+        "/home/aalvesne/coding/d/reggae/tmp/aruba/mixproj/src/cpp/maths.cpp \\",
+        "/home/aalvesne/coding/d/reggae/tmp/aruba/mixproj/headers/maths.hpp\n"];
+    dependenciesFromFile(depFileLines).shouldEqual(
         [ "/home/aalvesne/coding/d/reggae/tmp/aruba/mixproj/src/cpp/maths.cpp",
           "/home/aalvesne/coding/d/reggae/tmp/aruba/mixproj/headers/maths.hpp"]);
 }

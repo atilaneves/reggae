205ea247b77ea89ea348180ce704c7e996f8d452
Atila Neves <aalvesne@cisco.com>
2015-06-10 16:30:48 +0200

    Documentation in minimal_bootstrap.sh. Also fixed a bug in the script.

--- minimal_bootstrap.sh
+++ minimal_bootstrap.sh
@@ -1,4 +1,8 @@
-dmd -version=minimal -ofbin/reggae -Isrc -Ipayload -Jpayload/reggae src/reggae/{reggae_main,options}.d payload/reggae/{types,build,config}.d payload/reggae/rules/{common,defaults}.d
+# could also use rdmd like this:
+# rdmd --build-only -version=reggaelib -version=minimal -ofbin/reggae -Isrc -Ipayload -Jpayload/reggae reggaefile.d -b binary .
+# This script doesn't just so it doesn't have to depend on rdmd being available
+
+dmd -version=minimal -ofbin/reggae -Isrc -Ipayload -Jpayload/reggae src/reggae/{reggae_main,options,reggae}.d payload/reggae/{types,build,config}.d payload/reggae/rules/{common,defaults}.d
 
 cd bin
 ./reggae -b binary ..

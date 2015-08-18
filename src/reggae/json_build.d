b41430774443db51e84d59e02c40cc287a10b9f9
Atila Neves <aalvesne@cisco.com>
2015-08-18 14:59:27 +0200

    Properly failing UT for simple JSON build

---
+++ src/reggae/json_build.d
@@ -0,0 +1,8 @@
+module reggae.json_build;
+
+
+import reggae.build;
+
+Build jsonToBuild(in string json) {
+    return Build();
+}
--- tests/json_build/simple.d
+++ tests/json_build/simple.d
@@ -1,8 +1,20 @@
 module tests.json_build.simple;
 
 import reggae;
+import reggae.json_build;
 import unit_threaded;
 
-void testFoo() {
-    3.shouldEqual(5);
+immutable fooObjJson = `
+[
+  {
+    "outputs": ["foo.o"],
+    "command": "dmd -of$out -c $in",
+    "dependencies": [{"outputs": ["foo.d"], "command": "", "dependencies": [], "implicits": []}],
+    "implicits": []
+  }
+]`;
+
+void testFooObj() {
+    jsonToBuild(fooObjJson).shouldEqual(
+        Build(Target("foo.o", "dmd -of$out -c $in", Target("foo.d"))));
 }

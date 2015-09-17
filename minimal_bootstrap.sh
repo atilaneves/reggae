c4e1c85b9d11b55768025b4a573e32b3b5d93e24
BlackEdder <edder@tkwsping.nl>
2015-09-17 16:51:49 +0100

    Add shebang to the scripts
    bootstrap.sh needs to be run using bash

--- minimal_bootstrap.sh
+++ minimal_bootstrap.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # could also use rdmd like this:
 # rdmd --build-only -version=reggaelib -version=minimal -ofbin/reggae -Isrc -Ipayload -Jpayload/reggae reggaefile.d -b binary .
 # This script doesn't just so it doesn't have to depend on rdmd being available

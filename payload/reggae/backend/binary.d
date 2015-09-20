789413a940c80aed860efe32d44469a53a84dd78
Atila Neves <aalvesne@cisco.com>
2015-09-20 13:05:50 -0700

    Test passes but multiple makefile tests fail

--- payload/reggae/backend/binary.d
+++ payload/reggae/backend/binary.d
@@ -4,6 +4,7 @@ module reggae.backend.binary;
 import reggae.build;
 import reggae.range;
 import reggae.options;
+import reggae.dependencies;
 import std.algorithm;
 import std.range;
 import std.file: timeLastModified, thisExePath, exists;
@@ -177,12 +178,11 @@ private:
         auto file = File(depFileName);
         auto dependencies = file.byLine.map!(a => a.to!string).dependenciesFromFile;
 
-        if(!dependencies.empty){
-            if(dependencies.front.split(" ").any!(a => a.newerThan(target.outputsInProjectPath(options.projectPath)[0]))) {
-                executeCommand(target);
-                return true;
-            }
+        if(dependencies.any!(a => a.newerThan(target.outputsInProjectPath(options.projectPath)[0]))) {
+            executeCommand(target);
+            return true;
         }
+
         return false;
     }
 
@@ -210,12 +210,4 @@ bool newerThan(in string a, in string b) nothrow {
     } catch(Exception) { //file not there, so newer
         return true;
     }
-}
-
-string[] dependenciesFromFile(R)(R lines) @trusted if(isInputRange!R) {
-    return lines.
-        map!(a => a.replace(" \\", "")).
-        filter!(a => !a.empty).
-        map!(a => a.strip).
-        array[1..$];
 }

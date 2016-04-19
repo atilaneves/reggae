5aad6921ec9aa37ec82f6c7a08f116245ea67b84
Atila Neves <atila.neves@gmail.com>
2016-04-19 22:25:25 +0200

    Fix Travis CI build by disabling tests.it there (because of tup)

--- tests/utils.d
+++ tests/utils.d
@@ -3,22 +3,6 @@ module tests.utils;
 import reggae;
 import unit_threaded;
 
-void shouldThrowWithMessage(E)(lazy E expr, string expectedMsg,
-                               string file = __FILE__, size_t line = __LINE__) {
-    string msg;
-    bool threw;
-    try {
-        expr();
-    } catch(Exception ex) {
-        msg = ex.msg;
-        threw = true;
-    }
-
-    if(!threw) throw new UnitTestException(["Expression did not throw. Expected msg: " ~ msg],
-                                           file, line);
-    msg.shouldEqual(expectedMsg, file, line);
-}
-
 
 auto shouldExecuteOk(string[] args, string workDir,
                      string file = __FILE__, size_t line = __LINE__) {

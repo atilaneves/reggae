a97af7248250a4fa417617534cc2c5eafbacab68
Atila Neves <atila.neves@gmail.com>
2016-04-23 11:09:35 +0200

    Restored multiple_outputs test to do what the old cucumber one did

--- tests/it/buildgen/multiple_outputs.d
+++ tests/it/buildgen/multiple_outputs.d
@@ -17,17 +17,38 @@ unittest {
     auto options = testOptions(["-b", backend, inOrigPath("tests", "projects", "multiple_outputs")]);
     auto testPath = options.workingDir;
 
-    prepareTestBuild!"multiple_outputs.reggaefile_sep"(options);
+    enum module_ = "multiple_outputs.reggaefile_sep";
+    prepareTestBuild!module_(options);
     remove(buildPath(testPath, "protocol.d"));
     rename(buildPath(testPath, "reggaefile_sep.d"), buildPath(testPath, "reggaefile.d"));
     ["dmd", buildPath(testPath, "compiler.d")].shouldExecuteOk(testPath);
     ["dmd", buildPath(testPath, "translator.d")].shouldExecuteOk(testPath);
 
-    justDoTestBuild!"multiple_outputs.reggaefile_sep"(options);
+    justDoTestBuild!module_(options);
 
     auto appPath = inPath(testPath, "app");
     [appPath, "2"].shouldExecuteOk(testPath).shouldEqual(
         ["I call protoFunc(2) and get 4"]);
+
+    // ninja has problems with timestamp differences that are less than a second apart
+    if(backend == "ninja") {
+        import core.thread;
+        writelnUt("Sleeping before changing file");
+        Thread.sleep(1.seconds);
+    }
+
+    {
+        import std.stdio: File;
+        auto file = File(buildPath(testPath, "protocol.proto"), "w");
+        file.writeln("int protoFunc(int n) { return n * 3; }");
+    }
+
+    // rebuild
+    buildCmdShouldRunOk!module_(options);
+
+    [appPath, "3"].shouldExecuteOk(testPath).shouldEqual(
+        ["I call protoFunc(3) and get 9"]);
+
 }
 
 
@@ -38,16 +59,36 @@ unittest {
     auto backend = getValue!string;
     auto options = testOptions(["-b", backend, inOrigPath("tests", "projects", "multiple_outputs")]);
     auto testPath = options.workingDir;
+    enum module_ = "multiple_outputs.reggaefile_tog";
 
-    prepareTestBuild!"multiple_outputs.reggaefile_tog"(options);
+    prepareTestBuild!module_(options);
     remove(buildPath(testPath, "protocol.d"));
     rename(buildPath(testPath, "reggaefile_tog.d"), buildPath(testPath, "reggaefile.d"));
     ["dmd", buildPath(testPath, "compiler.d")].shouldExecuteOk(testPath);
     ["dmd", buildPath(testPath, "translator.d")].shouldExecuteOk(testPath);
 
-    justDoTestBuild!"multiple_outputs.reggaefile_tog"(options);
+    justDoTestBuild!module_(options);
 
     auto appPath = inPath(testPath, "app");
     [appPath, "2"].shouldExecuteOk(testPath).shouldEqual(
         ["I call protoFunc(2) and get 4"]);
+
+    // ninja has problems with timestamp differences that are less than a second apart
+    if(backend == "ninja") {
+        import core.thread;
+        writelnUt("Sleeping before changing file");
+        Thread.sleep(1.seconds);
+    }
+
+    {
+        import std.stdio: File;
+        auto file = File(buildPath(testPath, "protocol.proto"), "w");
+        file.writeln("int protoFunc(int n) { return n * 3; }");
+    }
+
+    // rebuild
+    buildCmdShouldRunOk!module_(options);
+
+    [appPath, "3"].shouldExecuteOk(testPath).shouldEqual(
+        ["I call protoFunc(3) and get 9"]);
 }

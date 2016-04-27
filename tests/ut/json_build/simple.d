87369e85ed6c2e20f8ccab34deb9d5e30d00574b
Atila Neves <atila.neves@gmail.com>
2016-04-27 16:56:01 +0200

    UT for optional fixed JSON target

--- tests/ut/json_build/simple.d
+++ tests/ut/json_build/simple.d
@@ -28,3 +28,45 @@ void testFooObj() {
     jsonToBuild("", fooObjJson).shouldEqual(
         Build(Target("foo.o", "dmd -of$out -c $in", Target("foo.d"))));
 }
+
+
+immutable optionalJson = `
+[
+  {
+    "type": "fixed",
+    "outputs": ["foo.o"],
+    "command": {"type": "shell", "cmd": "dmd -of$out -c $in"},
+    "dependencies": {
+        "type": "fixed",
+        "targets": [
+            {"type": "fixed",
+             "outputs": ["foo.d"],
+             "command": {},
+             "dependencies": {"type": "fixed", "targets": []},
+             "implicits": {"type": "fixed", "targets": []}}]},
+    "implicits": {"type": "fixed", "targets": []}
+  },
+  {
+    "type": "fixed",
+    "outputs": ["bar.o"],
+    "command": {"type": "shell", "cmd": "dmd -of$out -c $in"},
+    "dependencies": {
+        "type": "fixed",
+        "targets": [
+            {"type": "fixed",
+             "outputs": ["bar.d"],
+             "command": {},
+             "dependencies": {"type": "fixed", "targets": []},
+             "implicits": {"type": "fixed", "targets": []}}]},
+    "implicits": {"type": "fixed", "targets": []},
+    "optional": true
+  }
+]
+`;
+
+@("Optional target")
+unittest {
+    jsonToBuild("", optionalJson).shouldEqual(
+        Build(Target("foo.o", "dmd -of$out -c $in", Target("foo.d")),
+              optional(Target("bar.o", "dmd -of$out -c $in", Target("bar.d")))));
+}

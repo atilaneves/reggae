981a80fd65820f192190dba2d3899555cd345a6d
Atila Neves <atila.neves@gmail.com>
2016-05-08 20:52:09 +0200

    No more @HiddenTest

--- tests/it/buildgen/code_command.d
+++ tests/it/buildgen/code_command.d
@@ -6,24 +6,12 @@ void func(in string[], in string[]) {  }
 mixin build!(Target(`copy.txt`, &func, Target(`original.txt`)));
 
 
-//FIXME: hidden tests because of tup problem
-
-@HiddenTest
 @("code commands should fail with backends other than binary")
 @Values("ninja", "make", "tup")
+@AutoTags
 unittest {
     auto backend = getValue!string;
     writelnUt(backend);
-    doBuildFor!(__MODULE__)(testOptions(["-b", backend])).
-        shouldThrowWithMessage("Command type 'code' not supported for " ~ backend ~ " backend");
-}
-
-@HiddenTest
-@("code commands should fail with backends other than binary")
-@Values("ninja", "make")
-unittest {
-    auto backend = getValue!string;
-    writelnUt(backend);
-    doBuildFor!(__MODULE__)(testOptions(["-b", backend])).
+    doBuildFor!(__MODULE__)(testOptions(["-b", backend, newTestDir])).
         shouldThrowWithMessage("Command type 'code' not supported for " ~ backend ~ " backend");
 }

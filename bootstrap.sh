71c603778e301f2f8855eb71b9f490a424b09263
Atila Neves <atila.neves@gmail.com>
2017-04-06 22:47:38 +0200

    Diagnostics

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,7 +11,10 @@ COMP=${DMD:="dmd"}
 
 rm -rf bin
 
-$DMD -ofbin/reggae -Isrc -Ipayload -Jpayload/reggae src/reggae/*.d src/reggae/dub/*.d payload/reggae/backend/*.d payload/reggae/{options,reflect,config,build,types,sorting,dependencies,range,buildgen,package,ctaa,file}.d payload/reggae/rules/*.d payload/reggae/core/*.d payload/reggae/core/rules/*.d payload/reggae/dub/info.d
-cd bin
+echo "Compiling reggae"
+$COMP -ofbin/reggae -Isrc -Ipayload -Jpayload/reggae src/reggae/*.d src/reggae/dub/*.d payload/reggae/backend/*.d payload/reggae/{options,reflect,config,build,types,sorting,dependencies,range,buildgen,package,ctaa,file}.d payload/reggae/rules/*.d payload/reggae/core/*.d payload/reggae/core/rules/*.d payload/reggae/dub/info.d
+
+cd bin || exit 1
+"Running boostrapped reggae with backend $BACKEND"
 ./reggae -b $BACKEND ..
 $BACKEND

32b690f0bdefe163556074b01a15ce49410f2ffb
Atila Neves <aalvesne@cisco.com>
2017-05-22 12:35:04 -0700

    Failing dub UT for static library as source

--- tests/ut/rules/dub.d
+++ tests/ut/rules/dub.d
@@ -28,3 +28,43 @@ unittest {
                             []);
     actual.shouldEqual(expected);
 }
+
+
+
+@("dubTarget with static library source with env var in path")
+unittest {
+
+    import reggae.rules.dub: dubTarget;
+    import std.typecons: Yes, No;
+
+    DubInfo dubInfo;
+    dubInfo.packages = [DubPackage()];
+    dubInfo.packages[0].files = ["$KAL_EXT_LIB/liblua.a", "source/luad/foo.d"];
+    dubInfo.packages[0].importPaths = ["source"];
+    Target[] objects;
+    const actual = dubTarget!()(ExeName("app"),
+                                dubInfo,
+                                "-g -debug",
+                                Yes.main,
+                                No.allTogether,
+    );
+    auto compileTarget = Target("source.o",
+                                Command(CommandType.compile,
+                                        assocList(
+                                             [
+                                                 assocEntry("includes", ["-Isource", "-I"]),
+                                                 assocEntry("flags", ["-g", "-debug"]),
+                                                 assocEntry("DEPFILE", ["source.o.dep"])
+                                             ])),
+                                 [Target("source/luad/foo.d")],
+    );
+
+    string[] empty;
+    const expected = Target("app",
+                            Command(CommandType.link,
+                                    assocList([assocEntry("flags",
+                                                          empty)])),
+                            [compileTarget, Target("$KAL_EXT_LIB/liblua.a")]);
+
+    actual.shouldEqual(expected);
+}

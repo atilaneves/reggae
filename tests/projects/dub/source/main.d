5b06945433297f06df7d1e85ec2caa1b7ae65a62
Atila Neves <aalvesne@cisco.com>
2017-08-15 14:41:37 +0200

    Fix dub integration tests

--- tests/projects/dub/source/main.d
+++ tests/projects/dub/source/main.d
@@ -1,12 +1,8 @@
 import strings;
-import cerealed;
 import std.stdio;
 
 void main(string[] args) {
     writeln(import(`banner.txt`));
-    auto enc = Cerealiser();
-    enc ~= 4;
-    writeln(enc.bytes);
     writeln(string1);
 }
 

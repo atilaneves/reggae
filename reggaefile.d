6fe3860ee60b3a571e51678f8145e3df197ed21c
Atila Neves <atila.neves@gmail.com>
2017-08-18 11:52:19 +0200

    reggaefile formatting

--- reggaefile.d
+++ reggaefile.d
@@ -6,10 +6,16 @@ version(minimal) {
     //not the high level ones that do this automatically
     //This is a repetition in D of what's in minimal_bootstrap.sh
     enum flags = "-version=minimal -I$project/src -I$project/payload -J$project/payload/reggae";
-    enum srcs = [Target("src/reggae/reggae_main.d"), Target("src/reggae/reggae.d"), Target("payload/reggae/options.d"),
-                Target("payload/reggae/types.d"), Target("payload/reggae/build.d"),
-                Target("payload/reggae/config.d"), Target("payload/reggae/rules/common.d"),
-                Target("payload/reggae/rules/defaults.d")];
+    enum srcs = [
+        Target("src/reggae/reggae_main.d"),
+        Target("src/reggae/reggae.d"),
+        Target("payload/reggae/options.d"),
+        Target("payload/reggae/types.d"),
+        Target("payload/reggae/build.d"),
+        Target("payload/reggae/config.d"),
+        Target("payload/reggae/rules/common.d"),
+        Target("payload/reggae/rules/defaults.d"),
+    ];
     enum cmd = "dmd -of$out " ~ flags ~ " $in";
     enum main = Target("bin/reggae", cmd, srcs);
     mixin build!(main);

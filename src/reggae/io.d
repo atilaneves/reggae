79384da342180ff781912b386b897246442e0e44
Atila Neves <atila.neves@gmail.com>
2017-09-27 18:19:10 +0200

    Include time information in logging output

--- src/reggae/io.d
+++ src/reggae/io.d
@@ -3,6 +3,20 @@ module reggae.io;
 
 void log(O, T...)(auto ref O output, auto ref T args) {
     import std.functional: forward;
-    import std.datetime: Clock;
-    output.writeln("[Reggae]  ", Clock.currTime, "\t", forward!args);
+    output.writeln("[Reggae]  ", secondsSinceStartString, "s  ", forward!args);
+}
+
+private string secondsSinceStartString() @safe {
+    import std.string: rightJustify;
+    import std.conv: to;
+    return ("+" ~ (sinceStart / 1000.0).to!string).rightJustify(8, ' ');
+}
+
+private auto sinceStart() @safe {
+    import std.datetime: Clock, SysTime;
+    static SysTime startTime;
+
+    if(startTime == startTime.init) startTime = Clock.currTime;
+
+    return (Clock.currTime - startTime).total!"msecs";
 }

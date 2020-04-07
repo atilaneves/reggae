96cb61215c3359b4dadf3919a747ade6f46a1358
Atila Neves <atila.neves@gmail.com>
2020-04-07 15:28:02 +0200

    Fix UT that was failing locally

--- payload/reggae/sorting.d
+++ payload/reggae/sorting.d
@@ -1,20 +1,7 @@
 module reggae.sorting;
 
+
 import std.range: isInputRange;
-import std.path: pathSplitter, buildPath;
-import std.array: array;
-
-@safe:
-
-
-private string[] packageParts(string path) pure nothrow {
-    return () @trusted { return cast(string[])path.pathSplitter.array[0 .. $-1]; }();
-}
-
-private string filePackage(string path) pure nothrow {
-    import std.array: join;
-    return path.packageParts.join(".");
-}
 
 
 string[][] byPackage(R)(R files) if(isInputRange!R) {
@@ -27,9 +14,23 @@ string[][] byPackage(R)(R files) if(isInputRange!R) {
     return () @trusted { return packageToFiles.values; }();
 }
 
-/** Returns the path of the package corresponding to a file name.
+private string filePackage(string path) @safe pure nothrow {
+    import std.array: join;
+    return path.packageParts.join(".");
+}
+
+
+/**
+    Returns the path of the package corresponding to a file name.
  */
-string packagePath(in string fileName) pure {
+string packagePath(in string fileName) @safe pure {
     import std.algorithm: reduce;
+    import std.path: buildPath;
     return fileName.packageParts.reduce!((a, b) => buildPath(a, b));
+}
+
+private string[] packageParts(string path) @safe pure nothrow {
+    import std.path: pathSplitter;
+    import std.array: array;
+    return () @trusted { return cast(string[]) path.pathSplitter.array[0 .. $-1]; }();
 }

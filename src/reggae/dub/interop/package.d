8a11281481d61db248e7182094137fe6a49327b4
Atila Neves <atila.neves@gmail.com>
2020-10-16 19:34:07 +0200

    Fix dub packages with a reggaefile by fetching dependencies

--- src/reggae/dub/interop/package.d
+++ src/reggae/dub/interop/package.d
@@ -16,30 +16,32 @@ void writeDubConfig(T)(auto ref T output,
                        from!"std.stdio".File file) {
     import reggae.io: log;
     import reggae.dub.info: TargetType;
+    import reggae.dub.interop.exec: dubFetch;
 
     output.log("Writing dub configuration");
 
-    file.writeln("import reggae.dub.info;");
-
-    if(options.isDubProject) {
-
-        file.writeln("enum isDubProject = true;");
-        auto dubInfo = getDubInfo(output, options);
-        const targetType = dubInfo.packages.length
-            ? dubInfo.packages[0].targetType
-            : TargetType.sourceLibrary;
-
-        file.writeln(`const configToDubInfo = assocList([`);
-
-        const keys = () @trusted { return gDubInfos.keys; }();
-        foreach(config; keys) {
-            file.writeln(`    assocEntry("`, config, `", `, gDubInfos[config], `),`);
-        }
-        file.writeln(`]);`);
-        file.writeln;
-    } else {
+    if(!options.isDubProject) {
         file.writeln("enum isDubProject = false;");
+        return;
     }
+
+    dubFetch(output, options);
+
+    file.writeln("import reggae.dub.info;");
+    file.writeln("enum isDubProject = true;");
+    auto dubInfo = getDubInfo(output, options);
+    const targetType = dubInfo.packages.length
+        ? dubInfo.packages[0].targetType
+        : TargetType.sourceLibrary;
+
+    file.writeln(`const configToDubInfo = assocList([`);
+
+    const keys = () @trusted { return gDubInfos.keys; }();
+    foreach(config; keys) {
+        file.writeln(`    assocEntry("`, config, `", `, gDubInfos[config], `),`);
+    }
+    file.writeln(`]);`);
+    file.writeln;
 }
 
 

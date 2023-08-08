c30d877632577f92a0a341c2381860e0f2d078d7
Atila Neves <atila.neves@gmail.com>
2023-08-08 18:56:50 +0200

    Only run the test for dmd

--- tests/it/runtime/dependencies.d
+++ tests/it/runtime/dependencies.d
@@ -4,40 +4,41 @@ module tests.it.runtime.dependencies;
 import tests.it.runtime;
 
 
-// don't bother with binary since it's run with --norerun
-static foreach(backend; ["ninja", "make"]) {
-    @("reggaefile.imports." ~ backend)
-    @Tags(backend)
-    unittest {
-        with(immutable ReggaeSandbox()) {
-            writeFile(
-                "reggaefile.d",
-                q{
-                    import reggae;
-                    import constants;
-                    alias exe = executable!(ExeName(exeName), Sources!("source"));
-                    mixin build!exe;
-                }
-            );
-            writeFile("constants.d", q{enum exeName = "foo"; void lefoo() { }});
-            writeFile("source/main.d", "void main() { }");
+version(DigitalMars) {
+    // don't bother with binary since it's run with --norerun
+    static foreach(backend; ["ninja", "make"]) {
+        @("reggaefile.imports." ~ backend)
+        @Tags(backend)
+        unittest {
+            with(immutable ReggaeSandbox()) {
+                writeFile(
+                    "reggaefile.d",
+                    q{
+                        import reggae;
+                        import constants;
+                        alias exe = executable!(ExeName(exeName), Sources!("source"));
+                        mixin build!exe;
+                    }
+                );
+                writeFile("constants.d", q{enum exeName = "foo"; void lefoo() { }});
+                writeFile("source/main.d", "void main() { }");
 
-            runReggae("-b", backend);
-            mixin(backend).shouldExecuteOk;
+                runReggae("-b", backend);
+                mixin(backend).shouldExecuteOk;
 
-            // Rerunning reggae fails in this fake environment because there is no
-            // reggae binary to rerun. So this is a hack because it tracks whether or
-            // not we rerun reggae by having the call to reggae fail at runtime.
-            // We trigger this by writing to constants.d which should have correctly
-            // been identified as an implicit dependency of the reggaefile.
-            // If things actually worked, we'd assert that there's now a file named
-            // `bar`.
-            writeFile("constants.d", q{enum exeName = "bar"; void lefoo() { }});
-            auto lines = mixin(backend).shouldFailToExecute;
+                // Rerunning reggae fails in this fake environment because there is no
+                // reggae binary to rerun. So this is a hack because it tracks whether or
+                // not we rerun reggae by having the call to reggae fail at runtime.
+                // We trigger this by writing to constants.d which should have correctly
+                // been identified as an implicit dependency of the reggaefile.
+                // If things actually worked, we'd assert that there's now a file named
+                // `bar`.
+                writeFile("constants.d", q{enum exeName = "bar"; void lefoo() { }});
+                auto lines = mixin(backend).shouldFailToExecute;
+            }
         }
-    }
+ }
 }
-
 
 version(DigitalMars) {
     version(linux) {

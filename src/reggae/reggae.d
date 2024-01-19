7a7b4194fb0aef7a6929538653bfbff494a548f7
Atila Neves <atila.neves@gmail.com>
2024-01-19 15:33:48 +0100

    Only calculate deps for reggaefile when dub if needed

--- src/reggae/reggae.d
+++ src/reggae/reggae.d
@@ -278,31 +278,40 @@ private enum reggaeFileDubSelectionsJson =
 
 // dub support is needed at runtime, build and link with dub-as-a-library
 private Binary buildReggaefileDub(O)(auto ref O output, in Options options) {
-
-    import reggae.rules.common: objExt;
+    import reggae.io: log;
+    import reggae.build: Target, Command;
     import std.format: format;
-    import std.file: write, exists, readText;
     import std.path: buildPath;
     import std.algorithm: map, joiner;
     import std.range: chain, only;
-    import std.string: replace;
-    import std.array: array, replace;
+    import std.array: replace;
 
     // `options.getReggaeFileDependenciesDlang` depends on
     // `options.reggaeFileDepFile` existing, which means we need to
     // compile the reggaefile separately to get those dependencies
-    // *then* add any extra files to the dummy dub.sdl.  *Must* be
-    // done before attempting options.getReggaeFileDependenciesDlang
-    // below.
+    // *then* add any extra files to the dummy dub.sdl.
+    // *Must* be done before attempting
+    // options.getReggaeFileDependenciesDlang below.
     auto reggaefileObj = Binary(
-        "reggaefile" ~ objExt, // dummy name that doesn't matter
+        "_", // dummy name that doesn't matter
         [
             options.dCompiler,
             options.reggaeFilePath, "-o-", "-makedeps=" ~ options.reggaeFileDepFile,
             ]
         ~ importPaths(options),
         );
-    buildBinary(output, options, reggaefileObj);
+    auto target = Target(
+        options.reggaeFileDepFile,
+        Command(
+            (in string[] inputs, in string[] outputs) {
+                import reggae.io;
+                output.log("Calculating reggaefile dependencies");
+                buildBinary(output, options, reggaefileObj);
+            }
+        ),
+        options.reggaeFilePath,
+    );
+    buildTarget(options, target);
 
     // quote and separate with spaces for .sdl
     static stringsToSdlList(R)(R strings) {
@@ -349,8 +358,6 @@ private Binary buildReggaefileDub(O)(auto ref O output, in Options options) {
         libReggaeDubSdl,
     );
 
-
-
     // FIXME - use --compiler
     // The reason it doesn't work now is due to a test using
     // a custom compiler
@@ -358,6 +365,16 @@ private Binary buildReggaefileDub(O)(auto ref O output, in Options options) {
         getBuildGenName(options),
         ["dub", "build", "--nodeps", "--skip-registry=all"], // since we now depend on dub at buildgen runtime
     );
+}
+
+// build a target using reggae as a build system
+private void buildTarget(in Options options, imported!"reggae.build".Target target) {
+    import reggae.types: Backend;
+    import reggae.build: Build;
+
+    auto newOptions = options.dup;
+    newOptions.backend = Backend.binary;
+    runtimeBuild(newOptions, Build(target));
 }
 
 private void writeIfDiffers(O)(auto ref O output, in string path, in string contents) @safe {
